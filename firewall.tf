2cc9037e1d35ddddd021f6147e0f1c88aa54d490
Yurii Budnyi <ybudnyi@gmail.com>
2021-10-08 09:46:11 +0300

    add bastion host

--- firewall.tf
+++ firewall.tf
@@ -28,3 +28,19 @@ resource "google_compute_firewall" "webserver" {
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["webserver"]
 }
+
+resource "google_compute_firewall" "bastion" {
+  project = "nginx-328009"
+  name    = "bastion"
+  network = google_compute_network.webserver.id
+   allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["bastion"]
+}
